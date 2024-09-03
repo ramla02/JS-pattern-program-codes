0a6008d250edfc05fc22a33f70281adc225c0414
ramla02 <ramla252017@gmail.com>
2024-09-03 22:20:13 +0300

    pattern no 11

---
+++ 10-pattern11.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+let pattern = "";
+
+for (let i = 1; i <= 5; i++)
+{
+    for (let j = 1; j <= 3; j++)
+    {
+	pattern += i + " " + j + " ";
+    }
+    pattern += "\n";
+}
+console.log(pattern)
