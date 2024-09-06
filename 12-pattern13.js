d62085f79770fb184be1a9cf839515a78a8de7c2
ramla02 <ramla252017@gmail.com>
2024-09-07 02:06:05 +0300

    patternn no 13

---
+++ 12-pattern13.js
@@ -0,0 +1,27 @@
+
+
+let pattern = "";
+let n = 5;
+let x, y;
+
+for (let i = 1; i <= n; i++)
+{
+    x = i;
+    y = n - i + 1;
+
+    for (let j = 1; j <= n; j++)
+    {
+	if (j % 2 == 1)
+	{
+	    pattern += x.toString().padStart(3, " ")
+	}
+	else
+	{
+	    patern += y.toString().padStart(3, " ")
+	}
+	x = x + n;
+	y = y + n;
+    }
+    pattern += "\n";
+}
+console.log(pattern)
