8b95057b964461f13bd818c002dc78cba9489c72
ramla02 <ramla252017@gmail.com>
2024-09-09 14:18:56 +0300

    pattern no 16

---
+++ 15-pattern16.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+let pattern = "";
+
+let n = 5;
+for (let i = 1; i <= n; i++)
+{
+    for (let j = 1; j <= n; j++)
+    {
+        pattern += (i + j - 1) + "  ";
+    }
+    pattern += "\n";
+}
+console.log(pattern)
