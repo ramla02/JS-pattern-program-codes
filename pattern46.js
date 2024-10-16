033660d350e631db07447edb119b749ff1389975
ramla02 <ramla252017@gmail.com>
2024-10-16 04:02:46 +0300

    pattern no 46

---
+++ pattern46.js
@@ -0,0 +1,14 @@
+let pattern = "";
+let n = 5;
+let x = 2;
+
+for (let i = n; i >= 1; i--)
+{
+    for (let j = n; j >= i; j--)
+    {
+        pattern += x.toString().padStart(3, "  ");
+        x += 2;
+    }
+    pattern += "\n";
+}
+console.log(pattern)
