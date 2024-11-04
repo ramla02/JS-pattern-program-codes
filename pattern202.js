9284ee83354ece4e2ca582ee7720f16821757197
ramla02 <ramla252017@gmail.com>
2024-11-04 07:39:04 +0300

    pattern no 202

---
+++ pattern202.js
@@ -0,0 +1,16 @@
+/* Hexa number pattern */
+
+let pattern = "";
+let n = 4;
+let x = 1;
+
+for (let i = 1; i <= n; i++)
+{
+    for (let j = 1; j <= n; j++)
+    {
+        pattern += x.toString(16).padStart(3, "  ");
+        x++;
+    }
+    pattern += "\n";
+}
+console.log(pattern)
