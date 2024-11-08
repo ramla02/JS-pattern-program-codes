51b99c80ba6a6274d7cc5bd7a21ddb2c8783d8a5
ramla02 <ramla252017@gmail.com>
2024-11-08 12:17:10 +0300

    pattern no 206

---
+++ pattern206.js
@@ -0,0 +1,19 @@
+let pattern = "";
+let n = 5;
+
+for (let i = 1; i <= n; i++)
+{
+    for (let j = 1; j <= n; j++)
+    {
+        if (i <= n / 2)
+        {
+            pattern += "0  ";
+        }
+        else
+        {
+            pattern += "1  ";
+        }
+    }
+    pattern += "\n";
+}
+console.log(pattern)
